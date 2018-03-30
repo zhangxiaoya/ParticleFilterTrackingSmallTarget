4c8e6c21825b6382133048e34fe2615fa0d85dc2
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-03-30 11:49:27 +0800

    use Orientation in State class

--- ParticleFilterTrackingSmallTarget/State.h
+++ ParticleFilterTrackingSmallTarget/State.h
@@ -1,12 +1,22 @@
 #pragma once
 
-struct SpaceState
-{						  /* 状态空间变量 */
-	int centerX;          /* x坐标位置 */
-	int centerY;          /* y坐标位置 */
+#include "Orientation.h"
+
+/* 状态空间变量 */
+class SpaceState
+{
+public:
+	explicit SpaceState(Orientation orientation = Orientation(),
+						float v_xt = 0.0,
+						float v_yt = 0.0,
+						float at_dot = 0.0)
+			: _orientation(orientation),
+			  v_xt(v_xt),
+			  v_yt(v_yt),
+			  at_dot(at_dot) {}
+
+	Orientation _orientation;
 	float v_xt;           /* x方向运动速度 */
 	float v_yt;           /* y方向运动速度 */
-	int _halfWidthOfTarget;       /* x方向半窗宽 */
-	int _halfHeightOfTarget;      /* y方向半窗宽 */
 	float at_dot;         /* 尺度变换速度，粒子所代表的那一片区域的尺度变化速度 */
 };
